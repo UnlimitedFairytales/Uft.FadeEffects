20c07838aedbb13cdab12fd9e863474a86658063
HOME-20230701\Ether <etherealgarden@msn.com>
2025-04-08 01:48:58 +0900

    Sample追加

---
+++ Assets/Uft.FadeEffects/Samples~/FadeEffectSample/FadeEffectSample.cs
@@ -0,0 +1,39 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Uft.FadeEffects.Samples.FadeEffectSample
+{
+    public class FadeEffectSample : MonoBehaviour
+    {
+        FadeEffect _fadeEffect;
+        bool _isFadeIn = true;
+
+        void Start()
+        {
+            var scene = SceneManager.GetActiveScene();
+            var rootObjects = scene.GetRootGameObjects();
+            foreach (var obj in rootObjects)
+            {
+                if (obj.name == "FadeEffect")
+                {
+                    _fadeEffect = obj.GetComponent<FadeEffect>();
+                    break;
+                }
+            }
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.F))
+            {
+                UniTask.Action(async () =>
+                {
+                    _isFadeIn = !_isFadeIn;
+                    await _fadeEffect.StartFadeAsync(_isFadeIn);
+                    Debug.Log("StartFadeAsync complete");
+                })();
+            }
+        }
+    }
+}
