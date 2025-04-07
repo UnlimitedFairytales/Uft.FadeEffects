0d9443f3de29e90a8c57d368f1e7d4bf13822756
HOME-20230701\Ether <etherealgarden@msn.com>
2025-04-08 01:00:41 +0900

    Runtime記述

---
+++ Assets/Uft.FadeEffects/README.md
@@ -0,0 +1,10 @@
+﻿# Uft.FadeEffects
+
+This is some fade effect helpers.
+
+* Dependencies must be installed manually.
+
+## Dependencies
+
+- https://github.com/Cysharp/UniTask.git?path=src/UniTask/Assets/Plugins/UniTask
+- https://assetstore.unity.com/packages/tools/animation/dotween-hotween-v2-27676
---
+++ Assets/Uft.FadeEffects/Runtime/FadeConfig.cs
@@ -0,0 +1,15 @@
+﻿using DG.Tweening;
+using System;
+using UnityEngine;
+
+namespace Uft.FadeEffects
+{
+    [Serializable]
+    public class FadeConfig
+    {
+        public float inTime_sec = 1.0f;
+        public float outTime_sec = 1.0f;
+        public Color color = Color.black;
+        public Ease ease = Ease.OutQuad;
+    }
+}
