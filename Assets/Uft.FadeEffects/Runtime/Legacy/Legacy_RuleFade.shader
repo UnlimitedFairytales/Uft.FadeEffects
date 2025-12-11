b76b026afe4e49eb6e5d93b8bbe972f5dd90940b
SNOW\Ether <etherealgarden@msn.com>
2025-12-11 15:04:15 +0900

    RuleFade対応（Legacy）

---
+++ Assets/Uft.FadeEffects/Runtime/Legacy/Legacy_RuleFade.shader
@@ -0,0 +1,59 @@
+﻿Shader "Uft.FadeEffects/Legacy/RuleFade"
+{
+    Properties
+    {
+        _Amount      ("Amount",      Range(0, 1)) = 0
+        _Softness    ("Softness",    Range(0, 1)) = 0.5
+        _MainTex     ("Texture",              2D) = "white" {}
+        _SubTex      ("Sub Texture",          2D) = "white" {}
+        _SubTexColor ("Sub Texture Color", Color) = (1,1,1,1)
+        _RuleTex     ("Rule Texture",         2D) = "gray"  {}
+        [IntRange]
+        _Invert      ("Invert",      Range(0, 1))   = 0
+    }
+
+    SubShader
+    {
+        // ポストエフェクトのお約束
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag
+            #include "UnityCG.cginc"
+
+            sampler2D _MainTex;
+            sampler2D _SubTex;
+            sampler2D _RuleTex;
+            float _Amount;
+            float _Softness;
+            float4 _SubTexColor;
+            float _Invert;
+
+            fixed4 frag (v2f_img i) : SV_Target
+            {
+                // 元の色を取得
+                fixed4 fromCol = tex2D(_MainTex, i.uv);
+                fixed4 toCol   = tex2D(_SubTex, i.uv) * _SubTexColor;
+                float  rule    = tex2D(_RuleTex, i.uv).r;
+
+                // ルールをネガポジ反転するかどうかのオプション
+                rule = lerp(rule, 1.0 - rule, _Invert);
+
+                // ルールフェード計算 (Amountに応じてSoftnessの影響は三角形。0.5でそのまま。)
+                float t;
+                float s = _Softness * (1.0 - abs(2.0 * _Amount - 1.0));
+                if (s <= 0)
+                    t = step(rule, _Amount);
+                else
+                {
+                    t = smoothstep(rule - s, rule + s, _Amount);
+                }
+                return lerp(fromCol, toCol, t);
+            }
+            ENDCG
+        }
+    }
+}
--- Assets/Uft.FadeEffects/Runtime/SimplePostEffectConfig.cs
+++ Assets/Uft.FadeEffects/Runtime/SimplePostEffectConfig.cs
@@ -9,6 +9,11 @@ namespace Uft.FadeEffects
     {
         static readonly int AMOUNT_ID = Shader.PropertyToID("_Amount");
         static readonly int ANGLE_ID = Shader.PropertyToID("_Angle");
+        static readonly int SOFTNESS_ID = Shader.PropertyToID("_Softness");
+        static readonly int SUB_TEX_ID = Shader.PropertyToID("_SubTex");
+        static readonly int SUB_TEX_COLOR_ID = Shader.PropertyToID("_SubTexColor");
+        static readonly int RULE_TEX_ID = Shader.PropertyToID("_RuleTex");
+        static readonly int INVERT_ID = Shader.PropertyToID("_Invert");
 
         [SerializeField] Material _materialPrototype;
         Material _material; public Material Material => this._material;
@@ -25,6 +30,41 @@ namespace Uft.FadeEffects
         {
             get => this._angle;
             set => this._angle = Mathf.Clamp(value, -360, +360);
+        }
+
+        [SerializeField, Range(0, 1.0f)] float _softness;
+        public float Softness
+        {
+            get => this._softness;
+            set => this._softness = Mathf.Clamp(value, 0, 1.0f);
+        }
+
+        [SerializeField] Texture _subTex;
+        public Texture SubTex
+        {
+            get => this._subTex;
+            set => this._subTex = value;
+        }
+
+        [SerializeField] Color _subTexColor;
+        public Color SubTexColor
+        {
+            get => this._subTexColor;
+            set => this._subTexColor = value;
+        }
+
+        [SerializeField] Texture _ruleTex;
+        public Texture RuleTex
+        {
+            get => this._ruleTex;
+            set => this._ruleTex = value;
+        }
+
+        [SerializeField, Range(0, 1)] int _invert;
+        public int Invert
+        {
+            get => this._invert;
+            set => this._invert = value;
         }
 
         public void Setup()
@@ -47,6 +87,11 @@ namespace Uft.FadeEffects
 
             this._material.SetFloat(AMOUNT_ID, this.Amount);
             this._material.SetFloat(ANGLE_ID, this.Angle);
+            this._material.SetFloat(SOFTNESS_ID, this.Softness);
+            this._material.SetTexture(SUB_TEX_ID, this.SubTex);
+            this._material.SetColor(SUB_TEX_COLOR_ID, this.SubTexColor);
+            this._material.SetTexture(RULE_TEX_ID, this.RuleTex);
+            this._material.SetFloat(INVERT_ID, this.Invert);
         }
 
         public void Blit(RenderTexture src, RenderTexture dst)
