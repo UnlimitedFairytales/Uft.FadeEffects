eaa6f471af47b733bcf6d5453c7ea0c4cdce0757
SNOW\Ether <etherealgarden@msn.com>
2025-12-23 03:18:24 +0900

    サンプル側もRaycast確認できるように

--- Assets/Uft.FadeEffects/Samples~/FadeEffectSample/FadeEffectSample.cs
+++ Assets/Uft.FadeEffects/Samples~/FadeEffectSample/FadeEffectSample.cs
@@ -1,5 +1,9 @@
 using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 namespace Uft.FadeEffects.Samples.FadeEffectSample
@@ -7,10 +11,14 @@ namespace Uft.FadeEffects.Samples.FadeEffectSample
     public class FadeEffectSample : MonoBehaviour
     {
         [SerializeField] Sprite _sprite;
+        [SerializeField] TMP_Text _txtRaycastInfo;
+
+
         FadeEffect _fadeEffect;
         FadeEffect _imgFadeEffect;
         bool _isOnEffect;
         bool _isOnimgFadeEffect;
+        readonly List<RaycastResult> _results = new(32);
 
         void Start()
         {
@@ -53,6 +61,28 @@ namespace Uft.FadeEffects.Samples.FadeEffectSample
                     await this._imgFadeEffect.StartFadeAsync(newValue, 4.0f, new Color(0.5f, 0, 0, 1), new Color(0, 1, 0, 0.5f), this._sprite, DG.Tweening.Ease.OutBounce);
                     Debug.Log($"StartFadeAsync({newValue}) complete");
                 })();
+            }
+
+            if (EventSystem.current == null)
+            {
+                this._txtRaycastInfo.text = "EventSystem.current == null";
+            }
+            else
+            {
+                var eventData = new PointerEventData(EventSystem.current)
+                {
+                    position = Input.mousePosition, // 旧InputManager
+                };
+                this._results.Clear();
+                EventSystem.current.RaycastAll(eventData, this._results);
+                var sb = new StringBuilder(512);
+                sb.AppendLine($"UI Raycast hits: {this._results.Count}");
+                for (int i = 0; i < this._results.Count && i < 10; i++)
+                {
+                    var r = this._results[i];
+                    sb.AppendLine($"{i}: {r.gameObject.name} (module={r.module}, dist={r.distance})");
+                }
+                this._txtRaycastInfo.text = sb.ToString();
             }
         }
     }
